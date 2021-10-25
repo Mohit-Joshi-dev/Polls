a3dc50007a0eedcbc312f07ffe55b5d0d6881dd2
mohit-joshi-dev-envista <90506937+mohit-joshi-dev-envista@users.noreply.github.com>
2021-10-25 22:40:19 +0530

    Initial Commit

---
+++ polls/urls.py
@@ -0,0 +1,17 @@
+# from django.urls import path
+# from django.urls.resolvers import URLPattern
+# from django.urls import path
+
+# from . import views
+
+# urlpatterns = [
+#     path('',views.index, name='index'),
+# ]
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
